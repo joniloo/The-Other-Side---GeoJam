b2bedab63bfe0c18fcbd810f68467e6a369637b5
qqepijack <66687334+joniloo@users.noreply.github.com>
2021-03-25 20:15:27 +0200

    123

--- The Other Side/Assets/Skriptit/Kamera.cs
+++ The Other Side/Assets/Skriptit/Kamera.cs
@@ -11,12 +11,29 @@ public class Kamera : MonoBehaviour
     public PostProcessingProfile hellProfile;
     public PostProcessingProfile heavenProfile;
 
+    public float dampTime = 0.15f;
+    private Vector3 velocity = Vector3.zero;
+    public Transform target;
+
+    public float Y_axis = 0.5f;
+    public float X_axis = 0.5f;
+
     private void Start()
     {
         _camera = Camera.main;
 
     }
 
+    private void Update()
+    {
+        if (target)
+        {
+            Vector3 point = _camera.WorldToViewportPoint(target.position);
+            Vector3 delta = target.position - _camera.ViewportToWorldPoint(new Vector3(X_axis, Y_axis, point.z)); //(new Vector3(0.5, 0.5, point.z));
+            Vector3 destination = transform.position + delta;
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        }
+    }
 
     public void Portal()
     {
@@ -33,6 +50,8 @@ public class Kamera : MonoBehaviour
 
             GetComponent<PostProcessingBehaviour>().profile = hellProfile;
         }
+
+
 
     }
 }
